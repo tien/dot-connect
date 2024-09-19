2c1832f20b489102e3fd5d6c88d9f1b96571d49c
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-09-19 12:43:04 +1200

    refactor: allow passing unsupported wallets to options (#104)
    These will simply be ignored.

---
+++ .changeset/tender-pandas-double.md
@@ -0,0 +1,5 @@
+---
+"dot-connect": patch
+---
+
+Added support for passing unsupported wallets in the options configuration.
--- packages/dot-connect/src/index.ts
+++ packages/dot-connect/src/index.ts
@@ -1,12 +1,21 @@
 import "./elements/connection-button.js";
 import "./elements/connection-dialog.js";
 import { walletsOrAggregators } from "./stores.js";
-import type { SupportedWalletOrAggregator } from "./types.js";
+import {
+  type SupportedWalletOrAggregator,
+  supportedWalletsOrAggregators,
+} from "./types.js";
+import type { Wallet, WalletAggregator } from "@reactive-dot/core/wallets.js";
 
 export type Options = {
-  wallets: Array<SupportedWalletOrAggregator>;
+  wallets: Array<Wallet | WalletAggregator>;
 };
 
 export function registerDotConnect(options: Options) {
-  walletsOrAggregators.value = options.wallets;
+  walletsOrAggregators.value = options.wallets.filter(
+    (wallet): wallet is SupportedWalletOrAggregator =>
+      supportedWalletsOrAggregators.some(
+        (supported) => wallet instanceof supported,
+      ),
+  );
 }
