0b1c10bf4cbe03ae21951a7fb8bd739bdd7e0836
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-11-21 16:44:34 +1300

    refactor: wallet's logos (#209)

--- packages/dot-connect/src/wallets/ledger.ts
+++ packages/dot-connect/src/wallets/ledger.ts
@@ -1,19 +1,7 @@
-import type { WalletConfig, BaseWalletInfo } from "./types.js";
-import { html } from "lit";
+import type { BaseWalletInfo, WalletConfig } from "./types.js";
 
 export const ledger: WalletConfig<BaseWalletInfo> = {
   selector: (wallet) => wallet.id === "ledger",
   name: "Ledger",
-  logo: html`
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 768.91 669.35"
-      fill="currentcolor"
-    >
-      <path
-        d="M0,479.29v190.06h289.22V627.2H42.14V479.29H0z M726.77,479.29V627.2H479.69v42.14h289.22V479.29H726.77z M289.64,190.06
-	v289.22h190.05v-38.01H331.78V190.06H289.64z M0,0v190.06h42.14V42.14h247.08V0H0z M479.69,0v42.14h247.08v147.92h42.14V0H479.69z"
-      />
-    </svg>
-  `,
+  logo: new URL("../../assets/imgs/ledger.svg", import.meta.url),
 };
